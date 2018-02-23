f42b77ff458ec60fa9b81fda36657ee722ab6fb3
macziolkowski <m_ziolo_86@o2.pl>
2018-02-23 23:25:54 +0100

    Lets try using material design UI

--- www/js/script.js
+++ www/js/script.js
@@ -1,4 +1,4 @@
-angular.module('toDoApp', ['ngRoute'])
+angular.module('toDoApp', ['ngRoute', 'ngMaterial'])
 
 .config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
     $httpProvider.defaults.headers.delete = { "Content-Type": "application/json;charset=utf-8" };
